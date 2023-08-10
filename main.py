700c69f1c41a4a8d5e49f227caaf89ad1ab9d406
Joshua Hodges <j.b.h.0411@gmail.com>
2023-08-10 11:57:20 +0100

    changed filepath and removed gitignore(node)

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ def createCal(communityDays):
         calender.events.add(e)
     
     # print(calender.events)
-    with open('dist/community_day.ics','w') as my_file:
+    with open('calendars/community_day.ics','w') as my_file:
         my_file.writelines(calender.serialize_iter())
 
 
