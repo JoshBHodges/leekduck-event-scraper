dd06f27844697ec63d486b72ede13a499be22ca3
Joshua Hodges <j.b.h.0411@gmail.com>
2023-08-10 11:42:11 +0100

    fetch data and create ics first draft

--- main.py
+++ main.py
@@ -1,16 +1,45 @@
 #!/usr/bin/env python3
-"""
-Module Docstring
-"""
 
-__author__ = "Your Name"
-__version__ = "0.1.0"
-__license__ = "MIT"
-
+# import library
+from urllib.request import urlopen
+import json
+from ics import Calendar, Event
 
 def main():
-    """ Main entry point of the app """
-    print("hello world")
+    fetchEvents()
+
+def fetchEvents():
+    # store the URL in url as 
+    # parameter for urlopen
+    url = "https://raw.githubusercontent.com/bigfoott/ScrapedDuck/data/events.json"
+    
+    # store the response of URL
+    response = urlopen(url)
+    
+    # storing the JSON response 
+    # from url in data
+    data_json = json.loads(response.read())
+    
+    communityDays = [x for x in data_json if x['eventType'] == 'community-day']
+    # print the json response
+    # print(json.dumps(communityDays, indent=2))
+
+    createCal(communityDays)
+
+def createCal(communityDays):
+    calender = Calendar()
+
+    # create events from json data
+    for event in communityDays:
+        e = Event()
+        e.name = event["name"]
+        e.begin = event["start"]
+        e.end = event["end"]
+        calender.events.add(e)
+    
+    # print(calender.events)
+    with open('dist/community_day.ics','w') as my_file:
+        my_file.writelines(calender.serialize_iter())
 
 
 if __name__ == "__main__":
